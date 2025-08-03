393223e81f16d6d7d6f2a09e1e547632f4a2547c
BenyiUriel <benyi.uriel2006@gmail.com>
2025-08-03 16:58:15 -0600

    Se crea la el repository de persona

---
+++ src/main/java/com/bcss/shopall/repository/PersonaRepository.java
@@ -0,0 +1,9 @@
+package com.bcss.shopall.repository;
+
+import com.bcss.shopall.dto.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona,Long> {
+}
