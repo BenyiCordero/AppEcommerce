36c53b14cc8c5d13d5a02b11fef6c334ff597d6e
BenyiUriel <benyi.uriel2006@gmail.com>
2025-08-03 16:57:08 -0600

    Se crea la el repository de comprador

---
+++ src/main/java/com/bcss/shopall/repository/CompradorRepository.java
@@ -0,0 +1,9 @@
+package com.bcss.shopall.repository;
+
+import com.bcss.shopall.dto.Comprador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompradorRepository extends JpaRepository<Comprador,Long> {
+}
