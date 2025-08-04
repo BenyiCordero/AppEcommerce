0b4c14c35cebbee407dcf4987b324ec582aea535
BenyiUriel <benyi.uriel2006@gmail.com>
2025-08-04 09:32:47 -0600

    Se crea el modulo de MetodoPagoDetails y se realiza la conexion con Venta y MetodoPago

---
+++ src/main/java/com/bcss/shopall/domain/MetodoPagoDetails.java
@@ -0,0 +1,25 @@
+package com.bcss.shopall.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "MetodoPagoDetails")
+public class MetodoPagoDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idMetodoPagoDetails;
+    @ManyToOne
+    @JoinColumn(name = "id_metodo_pago", referencedColumnName = "idMetodoPago")
+    private MetodoPago metodoPago;
+    @ManyToOne
+    @JoinColumn(name = "id_venta", referencedColumnName = "idVenta")
+    private Venta venta;
+
+}
---
+++ src/main/java/com/bcss/shopall/service/MetodoPagoDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.bcss.shopall.service;
+
+
+import com.bcss.shopall.domain.MetodoPago;
+import com.bcss.shopall.domain.MetodoPagoDetails;
+import com.bcss.shopall.domain.Venta;
+import com.bcss.shopall.repository.MetodoPagoDetailsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MetodoPagoDetailsServiceImpl implements MetodoPagoDetailsService {
+
+    private final MetodoPagoDetailsRepository metodoPagoDetailsRepository;
+    private final VentaService ventaService;
+    private final MetodoPagoService metodoPagoService;
+
+    public MetodoPagoDetailsServiceImpl(MetodoPagoDetailsRepository metodoPagoDetailsRepository, ProductoService productoService, VentaService ventaService, MetodoPagoService metodoPagoService) {
+        this.metodoPagoDetailsRepository = metodoPagoDetailsRepository;
+        this.ventaService = ventaService;
+        this.metodoPagoService = metodoPagoService;
+    }
+
+    @Override
+    public MetodoPagoDetails crearMetodoPagoDetails(MetodoPagoDetails metodoPagoDetails) {
+        Optional<MetodoPago> metodoPago = metodoPagoService.buscarPorId(metodoPagoDetails.getMetodoPago().getIdMetodoPago());
+        Optional<Venta> venta = ventaService.buscarPorId(metodoPagoDetails.getVenta().getIdVenta());
+        metodoPagoDetails.setMetodoPago(metodoPago.get());
+        metodoPagoDetails.setVenta(venta.get());
+        return metodoPagoDetailsRepository.save(metodoPagoDetails);
+    }
+
+    @Override
+    public List<MetodoPagoDetails> buscarMetodoPagoDetailsPorVenta(Long idVenta) {
+        return metodoPagoDetailsRepository.findByVenta(ventaService.buscarPorId(idVenta).get());
+    }
+}
